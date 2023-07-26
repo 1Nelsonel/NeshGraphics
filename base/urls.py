0a74c9f6c6faf8564ba6456e99c3390814b1778f
Nelson Mokoro <nelsonmokoro37@gmail.com>
2023-07-26 10:14:48 +0300

    success

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,25 @@
+# my_project/urls.py
+from django.shortcuts import render
 from django.urls import path
+from django.conf.urls import handler404
 from . import views
-
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('service/', views.service, name='service'),
+    path('blogs/', views.blogs, name='blogs'),
+    path('blog/', views.blog, name='blog'),
+    path('faq/', views.faq, name='faq'),
+    path('contact/', views.contact, name='contact'),
+    path('team/', views.teams, name='teams'),
+    path('team/', views.team, name='team'),
 ]
+
+# This view will handle 404 errors
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
+
+# Assign the custom_404_view to handle 404 errors
+handler404 = custom_404_view
--- base/views.py
+++ base/views.py
@@ -1,7 +1,63 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+# ===============================================================
+# home
+# ===============================================================
 def home(request):
     context = {}
     return render(request, 'base/home.html', context)
+
+# ===============================================================
+# about
+# ===============================================================
+def about(request):
+    context = {}
+    return render(request, 'base/about.html', context)
+
+# ===============================================================
+# services
+# ===============================================================
+def services(request):
+    context = {}
+    return render(request, 'base/services.html', context)
+
+def service(request):
+    context = {}
+    return render(request, 'base/service.html', context)
+
+# ===============================================================
+# contact
+# ===============================================================
+def contact(request):
+    context = {}
+    return render(request, 'base/contact.html', context)
+
+# ===============================================================
+# faq
+# ===============================================================
+def faq(request):
+    context = {}
+    return render(request, 'base/faq.html', context)
+
+# ===============================================================
+# team
+# ===============================================================
+def teams(request):
+    context = {}
+    return render(request, 'base/team.html', context)
+
+def team(request):
+    context = {}
+    return render(request, 'base/team-single.html', context)
+
+# ===============================================================
+# blog
+# ===============================================================
+def blogs(request):
+    context = {}
+    return render(request, 'base/blogs.html', context)
+
+def blog(request):
+    context = {}
+    return render(request, 'base/blog.html', context)
